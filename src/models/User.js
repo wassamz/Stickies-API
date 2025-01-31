90f209868234a12d2b2f46a291b5c092e6b520c4
Wassam <wassam@wassam.com>
2025-01-31 12:21:35 -0500

    Replace bcryptjs with bcrypt for password hashing and update vitest dependency

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcrypt";
 import { model, Schema } from "mongoose";
 
 const UserSchema = new Schema(
@@ -26,8 +26,8 @@ const UserSchema = new Schema(
 UserSchema.pre("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
     try {
-      const salt = await bcryptjs.genSalt(10);
-      this.password = await bcryptjs.hash(this.password, salt);
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
     } catch (error) {
       return next(error);
     }
@@ -36,7 +36,7 @@ UserSchema.pre("save", async function (next) {
 });
 
 UserSchema.methods.comparePassword = async function (password) {
-  return await bcryptjs.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 const User = model("User", UserSchema);
 
