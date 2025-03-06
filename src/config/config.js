b55bdd2a914641b635176436609fcf3bd79c27b4
Wassam <wassam@wassam.com>
2025-03-06 17:40:10 -0500

    Add OTP settings and rate limiter configuration to environment and config files

--- src/config/config.js
+++ src/config/config.js
@@ -13,6 +13,10 @@ const config = {
   jwtAccessExpireTime: process.env.JWT_ACCESS_EXPIRE_TIME,
   jwtRefreshExpireTime: process.env.JWT_REFRESH_EXPIRE_TIME,
 
+  rateLimitWindowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS),
+  rateLimitMaxAttempts: parseInt(process.env.RATE_LIMIT_MAX_ATTEMPTS),
+  rateLimitSkipSuccess: process.env.RATE_LIMIT_SKIP_SUCCESS === 'true',
+
   allowedOrigin: process.env.FRONTEND_DOMAIN,
 
   dbUser: process.env.DB_USERNAME,
@@ -24,6 +28,10 @@ const config = {
   pwdMaxRetryAttempts: process.env.PWD_MAX_RETRY_ATTEMPTS,
   pwdMaxForgetRetryAttempts: process.env.PWD_MAX_FORGET_RETRY_ATTEMPTS,
   pwdResetOTPExpireTime: process.env.PWD_RESET_OTP_EXPIRE_TIME,
+
+  otpLength: parseInt(process.env.OTP_LENGTH || '4'),
+  otpMin: Math.pow(10, parseInt(process.env.OTP_LENGTH || '4') - 1),
+  otpMax: Math.pow(10, parseInt(process.env.OTP_LENGTH || '4')) - 1,
 
   smtpFromName: process.env.SMTP_FROM_NAME,
   smtpEmail: process.env.SMTP_EMAIL,
